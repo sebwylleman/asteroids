971c7ad0618f7060a612386901cb3692723fb014
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2025-03-02 09:20:47 +0000

    player sprite drawm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
@@ -8,6 +9,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -15,7 +19,7 @@ def main():
                 return
 
         screen.fill("black")
-
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS

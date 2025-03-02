71afe839e3a36e1c5f40434f267b33a479c922d2
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2025-03-02 09:22:04 +0000

    refactor

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():

d9b0b6ccb3ca0b87c892a21ea49680bde93eba3d
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2025-03-03 07:21:21 +0000

    refactor

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -16,9 +21,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
 
         pygame.display.flip()
 
@@ -28,4 +35,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
